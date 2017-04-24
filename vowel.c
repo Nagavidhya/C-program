0d7d8df300d1340786e14252bec6de94d99f1d82
Nagavidhya <vidhyajanu524@gmail.com>
2017-04-24 14:32:20 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-void main()
+int main()
 {
 char ch;
 int lowercase_Vowel,uppercase_Vowel;
 printf("please Enter an alphabet: \n");
-scanf("%d",&ch);
+scanf("%c",&ch);
 lowercase_Vowel = (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u');
 uppercase_Vowel = (ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U');
 if(lowercase_Vowel || uppercase_Vowel)
