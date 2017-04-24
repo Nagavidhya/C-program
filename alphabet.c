57427b08d1a7a4676f11e579b70b9c23f7da0886
Nagavidhya <vidhyajanu524@gmail.com>
2017-04-24 15:04:31 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char a;
+scanf("%c",&a);
+if((a>='a' && a<='z')||(a>='A' && a<='Z'))
+{
+printf("%c is an alphabet",a);
+}
+else
+{
+printf("%c is not an alphabet",a);
+}
+return 0;
+}
+
+
