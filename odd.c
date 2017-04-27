cdfaee2597b5a25dcb9d2920b7d2b60fb65c04c1
Nagavidhya <vidhyajanu524@gmail.com>
2017-04-27 13:37:49 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,i;
+clrscr();
+scanf("%d%d",&a,&b);
+printf("Diasplays the number between %d and %d:"a,b);
+for(i=0;i<b;i++)
+{
+if(i%2!=0)
+{
+printf("%d",&i);
+}
+getch();
+}
+}
